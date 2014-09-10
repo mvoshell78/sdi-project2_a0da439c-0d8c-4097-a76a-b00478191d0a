ce74a801cc934af15cdf2d4e7036e5d1bf3d1a57
mvoshell78 <michael.voshell@yahoo.com>
2014-09-10 16:37:14 -0400

    redid procedure and prompt

--- voshell_michael_assignment2.js
+++ voshell_michael_assignment2.js
@@ -12,28 +12,39 @@ console.log( " Our Computer systems at work crashed on Sat ")
 console.log( "it's been a very hectic week to say the least because of it ")
 console.log( " I have to drive to Laurel, DE over " + tripTime + " hours round trip, every nght")
 
-
-
-// Procedure with prompt
-
-var myPrompt = prompt ("Hey, Mich it's " + mhName + " is the system back up?");
- 
- 
-    // function for procedure with prompt
-    // if system is up responds thats awesome
-    // if it's no respondes that stinks 
-var systemUp = function (myPrompt) {
-       if (myPrompt === "yes" ){
-        console.log ('Thats awesome! it\'ll save us a bunch of work tonight ')
-        } else {
-        console.log ("That really stinks, more work for us tonight" )
+//  Prompt Arguement
+var systemUp = function () {
+    var myPrompt = prompt("Hey, Mich it's " + mhName + " is the system back up?"); { 
+        if (myPrompt === "yes"){
+            //output 1 if yes
+            console.log ('Thats awesome! it\'ll save us a bunch of work tonight ')
+            } else {
+                if (myPrompt === "no") {
+                    // output 2 if no
+                    console.log ("That really stinks, more work for us tonight")
+                //code
+                } else {
+                    console.log ( "Stop playing around we have work to do" ) }
         
-        return
-        
+                };
+            };
         };
-};
+ 
+ // Boolean Function 
+ 
+        // arguemnet conditional
+    // while-loop
+  //  var dayOfTheWeek = myConfirm;    
+  //  while (dayOfTheWeek < 4) {
+            //console.log (dayOfTheWeek + " day of the system not working is " + dayOfTheWeek + " day(s) too many")
+            
+          //  dayOfTheWeek++;
+                
+                
+          //  };
+      
+//};
 
+ 
 
-    systemUp(myPrompt)
-
-    
+systemUp()
